019bd1694e8684dc0c14addbe4d72f4814859908
Jhonny Lin <67341179+Jlin72@users.noreply.github.com>
2021-01-25 13:04:48 -0500

    Added signup javascript code
    Tested it locally it works in creating a user to the database and once the user is created it will redirect the user to /members

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const createformEl = document.querySelector('.signup');
+const createInputEmailEl = document.querySelector("#email-input");
+const createInputPasswordEl = document.querySelector("#password-input");
+
+createformEl.addEventListener('submit', (e)=> {
+    console.log('hello');
+    e.preventDefault();
+    const newUser = {
+        email: createInputEmailEl.value.trim(),
+        password: createInputPasswordEl.value.trim()
+    };
+    if(!newUser.email || !newUser.password) {
+        return;
+    }
+
+
+    fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    }).then(()=> {
+        // window.location.replace('/members');
+        console.log(newUser)
+    })
+});
