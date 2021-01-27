74b2cf508841a4657da5d6de132ea8276ffbbddb
Karl Tulio <kltulio@up.edu.ph>
2021-01-26 21:33:40 -0500

    Initial commit for sign up styling

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
-const createformEl = document.querySelector('#signUp'); //Change this to the signup correct id or class
-const createInputEmailEl = document.querySelector('#email-input'); //Change this to the signup correct id or class
-const createInputPasswordEl = document.querySelector('#password-input'); //Change this to the signup correct id or class
-console.log("Email: ",createInputEmailEl.value);
-console.log("Password: ",createInputPasswordEl.value);
-createformEl.addEventListener('click', (e)=> {
-    e.preventDefault();
-    console.log("Initial log for signup.js");
-=======
-const createformEl = document.querySelector('.signup');
+const createformEl = document.querySelector('#signup');
 const createInputEmailEl = document.querySelector("#email-input");
 const createInputPasswordEl = document.querySelector("#password-input");
 
 createformEl.addEventListener('submit', (e)=> {
+    console.log("IS THIS RUNNING");
     e.preventDefault();
->>>>>>> develop
     const newUser = {
         email: createInputEmailEl.value.trim(),
         password: createInputPasswordEl.value.trim()
     };
-<<<<<<< HEAD
-    console.log(newUser);
-    if(!newUser.email || !newUser.password) {
-        return;
-    }
-=======
     if(!newUser.email || !newUser.password) {
         return;
     }
 
 
->>>>>>> develop
     fetch('/api/signup', {
         method: 'POST',
         headers: {
@@ -42,8 +25,4 @@ createformEl.addEventListener('submit', (e)=> {
         // window.location.replace('/members');
         console.log(newUser)
     })
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> develop
